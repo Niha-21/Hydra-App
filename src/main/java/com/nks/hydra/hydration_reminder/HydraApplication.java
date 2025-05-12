9c063325946a6e2016764914707fdde0d240a673
Niha-21 <nihakamalnk@gmail.com>
2025-05-13 00:21:53 +0530

    initial commit

---
+++ src/main/java/com/nks/hydra/hydration_reminder/HydraApplication.java
@@ -0,0 +1,13 @@
+package com.nks.hydra.hydration_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nks/hydra/hydration_reminder/HydraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nks.hydra.hydration_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
